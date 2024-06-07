dcf2862b6db05c4b1066872474736443940e2deb
SergioMelado <sergio.melado@soprasteria.com>
2024-06-07 08:27:54 +0200

    OrderControllerTest completed, ProductControllerTest needs to be fixed

---
+++ src/test/java/com/tour_of_heroes/api/apps/shop/OrderControllerTest.java
@@ -0,0 +1,137 @@
+package com.tour_of_heroes.api.apps.shop;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tour_of_heroes.api.controllers.OrderController;
+import com.tour_of_heroes.api.shop.domain.contracts.services.OrderService;
+import com.tour_of_heroes.api.shop.domain.entities.Order;
+import com.tour_of_heroes.api.shop.domain.entities.Product;
+import com.tour_of_heroes.api.shop.domain.entities.ProductOrder;
+import com.tour_of_heroes.api.shop.infrastructure.dto.InOrderDTO;
+import com.tour_of_heroes.api.shop.infrastructure.dto.OutOrderDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(OrderController.class)
+public class OrderControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService srv;
+
+    private String urlTemplate;
+
+    @BeforeEach
+    void setUp() {
+        urlTemplate = "/api/shop/orders";
+    }
+
+    @Test
+    void whenGetOneOrder_thenControlCorrectFlow() throws Exception {
+
+        Order order = new Order();
+
+        when(srv.getOne(any(UUID.class))).thenReturn(Optional.of(order));
+
+        mockMvc.perform(get(urlTemplate + "/{id}", order.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(order.getId().toString()))
+                .andExpect(jsonPath("$.expedition_date").value(order.getExpeditionDate().toString()))
+                .andDo(print());
+
+        verify(srv, times(1)).getOne(any(UUID.class));
+    }
+
+    @Test
+    void whenCreateOrder_thenControlCorrectFlow() throws Exception {
+
+        List<Product> productList = List.of(
+                new Product("Test", "Product Test", 100.0)
+        );
+
+        InOrderDTO inOrderDTO = new InOrderDTO();
+        inOrderDTO.setProductList(productList);
+        Order order = new Order();
+        OutOrderDTO outOrderDTO = OutOrderDTO.from(order);
+
+        when(srv.add(any(Order.class))).thenReturn(order);
+
+        mockMvc.perform(post(urlTemplate)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(inOrderDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(outOrderDTO.getId().toString()))
+                .andExpect(jsonPath("$.expedition_date").value(outOrderDTO.getExpeditionDate()))
+                .andDo(print());
+
+        verify(srv, Mockito.times(1))
+                .add(Mockito.any(Order.class));
+    }
+
+    @Test
+    void whenGetAllOrders_thenControlCorrectFlow() throws Exception {
+
+        List<Order> orderList = List.of(
+                new Order(),
+                new Order(),
+                new Order()
+        );
+
+        when(srv.getAll()).thenReturn(orderList);
+
+        mockMvc.perform(get(urlTemplate))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andDo(print());
+
+        verify(srv, Mockito.times(1)).getAll();
+    }
+
+    @Test
+    void whenGetProductsFromOrder_thenControlCorrectFlow() throws Exception {
+
+        int id = 1;
+        Order order = new Order();
+        Product product = new Product(id, "ProductTest1", 125.0, "ProductTest1");
+
+        List<ProductOrder> productOrderList = List.of(
+
+                new ProductOrder(order, product)
+        );
+
+        order.setProductOrderList(productOrderList);
+
+        when(srv.getOne(any(UUID.class))).thenReturn(Optional.of(order));
+
+        mockMvc.perform(get(urlTemplate + "/{id}" + "/products", order.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andDo(print());
+    }
+}
---
+++ src/test/java/com/tour_of_heroes/api/apps/shop/ProductControllerTest.java
@@ -0,0 +1,157 @@
+package com.tour_of_heroes.api.apps.shop;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tour_of_heroes.api.controllers.ProductController;
+import com.tour_of_heroes.api.heroes.domain.entities.Hero;
+import com.tour_of_heroes.api.shared.exceptions.NotFoundException;
+import com.tour_of_heroes.api.shop.domain.contracts.services.ProductService;
+import com.tour_of_heroes.api.shop.domain.entities.Order;
+import com.tour_of_heroes.api.shop.domain.entities.Product;
+import com.tour_of_heroes.api.shop.infrastructure.dto.InProductDTO;
+import com.tour_of_heroes.api.shop.infrastructure.dto.OutProductDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ProductController.class)
+public class ProductControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService srv;
+
+    private String urlTemplate;
+
+    @BeforeEach
+    void setUp() {
+        urlTemplate = "/api/shop/products";
+    }
+
+    @Test
+    void whenGetOneProduct_thenControlCorrectFlow() throws Exception {
+
+        int id = 1;
+        Product product = new Product(id, "ProductTest", 200.0, "ProductTestDescription");
+
+        when(srv.getOne(anyInt())).thenReturn(Optional.of(product));
+
+        mockMvc.perform(get(urlTemplate + "/{id}", product.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(product.getId()))
+                .andExpect(jsonPath("$.name").value(product.getName()))
+                .andExpect(jsonPath("$.price").value(product.getPrice()))
+                .andExpect(jsonPath("$.description").value(product.getDescription()))
+                .andDo(print());
+
+        verify(srv, times(1)).getOne(anyInt());
+    }
+
+    @Test
+    void whenGetAllProducts_thenControlCorrectFlow() throws Exception {
+
+        List<Product> productList = List.of(
+
+                new Product("Product1", "Product1Description", 100.0),
+                new Product("Product2", "Product2Description", 100.0),
+                new Product("Product3", "Product3Description", 100.0)
+        );
+
+        when(srv.getAll()).thenReturn(productList);
+
+        mockMvc.perform(get(urlTemplate))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andDo(print());
+
+        verify(srv, Mockito.times(1)).getAll();
+    }
+
+    @Test
+    void whenCreateProduct_thenControlCorrectFlow() throws Exception {
+
+        InProductDTO inProductDTO = new InProductDTO("ProductTest", 100.0, "ProductTestDescription");
+        Product product = InProductDTO.from(inProductDTO);
+        OutProductDTO outProductDTO = OutProductDTO.from(product);
+
+        when(srv.add(any(Product.class))).thenReturn(product);
+
+        mockMvc.perform(post(urlTemplate)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(inProductDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(outProductDTO.getId()))
+                .andExpect(jsonPath("$.name").value(outProductDTO.getName()))
+                .andExpect(jsonPath("$.price").value(outProductDTO.getPrice()))
+                .andExpect(jsonPath("$.description").value(outProductDTO.getDescription()))
+                .andDo(print());
+
+        verify(srv, Mockito.times(1))
+                .add(Mockito.any(Product.class));
+    }
+
+    @Test
+    void whenUpdatingProduct_thenControlCorrectFlow() throws Exception {
+
+        int id = 1;
+        InProductDTO inProductDTO = new InProductDTO("ProductTest", 150.0, "ProductTestDescription");
+        Product product = new Product(id, "ProductTest",100.0, "ProductTestDescription");
+        OutProductDTO outProductDTO = OutProductDTO.from(product);
+
+        when(srv.modify(any(Product.class))).thenReturn(product);
+
+        mockMvc.perform(patch(urlTemplate + "/{id}", id)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(inProductDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(outProductDTO.getId()))
+                .andExpect(jsonPath("$.name").value(outProductDTO.getName()))
+                .andExpect(jsonPath("$.price").value(outProductDTO.getPrice()))
+                .andExpect(jsonPath("$.description").value(outProductDTO.getDescription()))
+                .andDo(print());
+
+        verify(srv, Mockito.times(1))
+                .modify(Mockito.any(Product.class));
+    }
+
+    @Test
+    void whenRemoveProductById_thenControlCorrectFlow() throws Exception {
+
+        int id = 1;
+        Product product = new Product(id, "ProductTest", 100.0, "ProductTestDescription");
+
+        when(srv.getOne(id)).thenReturn(Optional.of(product));
+        doNothing().when(srv).deleteById(id);
+
+        mockMvc.perform(delete(urlTemplate + "/{id}", id))
+                .andExpect(status().isNoContent());
+
+        verify(srv, Mockito.times(1)).getOne(id);
+        verify(srv, Mockito.times(1)).deleteById(id);
+    }
+}
