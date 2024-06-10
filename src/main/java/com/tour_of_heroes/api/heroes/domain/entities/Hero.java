b34624b7d2d223bb79a809fbe0f01ac7f9b3e6bf
SergioMelado <sergio.melado@soprasteria.com>
2024-06-10 11:03:52 +0200

    HeroRepositoryTest completed

--- src/main/java/com/tour_of_heroes/api/heroes/domain/entities/Hero.java
+++ src/main/java/com/tour_of_heroes/api/heroes/domain/entities/Hero.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 @Entity
 @Table(name = "HEROES")
 public class Hero {
--- src/test/java/com/tour_of_heroes/api/apps/heroes/context/HeroRepositoryTest.java
+++ src/test/java/com/tour_of_heroes/api/apps/heroes/context/HeroRepositoryTest.java
@@ -6,11 +6,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.AUTO_CONFIGURED)
 public class HeroRepositoryTest {
 
     @Autowired
@@ -36,11 +41,63 @@ public class HeroRepositoryTest {
 
     @Test
     @DisplayName("test for get Hero List")
-    public void givenHeroList_whenFindAll_thenHeroList(){
-
-        Hero heroOne = new Hero("Pepo");
+    public void givenHeroList_whenFindAll_thenHeroList() {
+        Hero heroOne = new Hero("Pepe");
         Hero heroTwo = new Hero("Pepa");
 
-        
+        repository.save(heroOne);
+        repository.save(heroTwo);
+
+        // When : Action of behavious that we are going to test
+        List<Hero> heroList = repository.findAll();
+
+        // Then : Verify the output
+        assertThat(heroList).isNotEmpty();
+        assertThat(heroList.size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("test for get Hero By Id")
+    public void givenHeroObject_whenFindById_thenReturnHeroObject() {
+        // Given : Setup object or precondition
+        hero = repository.save(hero);
+
+        // When : Action of behavious that we are going to test
+        Hero getHero = repository.findById(hero.getId()).get();
+
+        // Then : Verify the output
+        assertThat(getHero).isNotNull();
+    }
+
+    @Test
+    @DisplayName("test for get Hero update operation")
+    public void givenHeroObject_whenUpdate_thenHeroObject() {
+
+        // Given: Setup object or precondition
+        hero = repository.save(hero);
+
+        // When: Action or behavior that we are going to test
+        Hero getHero = repository.findById(hero.getId()).get();
+
+        String updatedName = "Pepa";
+        getHero.setName(updatedName);
+
+        Hero updatedHero = repository.save(getHero);
+
+        // Then: Verify the output or expected result
+        assertThat(updatedHero).isNotNull();
+        assertThat(updatedHero.getName()).isEqualTo(updatedName);
+    }
+
+    @Test
+    @DisplayName("test for delete hero operation")
+    public void givenHeroObject_whenDelete_thenRemoveHero() {
+
+        hero = repository.save(hero);
+
+        repository.deleteById(hero.getId());
+        Optional<Hero> deleteHero = repository.findById(hero.getId());
+
+        assertThat(deleteHero).isEmpty();
     }
 }
